e11f464829a4757b7270ef50d59ccdbb3930b967
Mudia Osagie <mudia.personal@protonmail.com>
2024-12-26 22:58:35 +0000

    fix: add weatherinfo component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import EmotionLogger from "../components/EmotionLogger";
+import WeatherInfo from "../components/WeatherInfo";
 import PropTypes from "prop-types";
 
 function Home({ pin }) {
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Mind App</h1>
+      <WeatherInfo />
       <EmotionLogger pin={pin} />
     </div>
   );
