c02e67f6a706574301823a31de4e464247988710
Mudia Osagie <mudia.personal@protonmail.com>
2024-12-27 22:04:02 +0000

    feat: add Catppuccin Latte theme to TailwindCSS#

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    colors: {
+      latte: {
+        base: "#eff1f5",
+        mantle: "#e6e9ef",
+        crust: "#dce0e8",
+        text: "#4c4f69",
+        subtext1: "#5c5f77",
+        subtext0: "#6c6f85",
+        overlay2: "#7c7f93",
+        overlay1: "#8c8fa1",
+        overlay0: "#9ca0b0",
+        surface2: "#acb0be",
+        surface1: "#bcc0cc",
+        surface0: "#ccd0da",
+        lavender: "#7287fd",
+        blue: "#1e66f5",
+        sapphire: "#209fb5",
+        sky: "#04a5e5",
+        teal: "#179299",
+        green: "#40a02b",
+        yellow: "#df8e1d",
+        peach: "#fe640b",
+        maroon: "#e64553",
+        red: "#d20f39",
+        mauve: "#8839ef",
+        pink: "#ea76cb",
+        flamingo: "#dd7878",
+        rosewater: "#dc8a78",
+      },
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
